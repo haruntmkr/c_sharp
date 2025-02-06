bea9cb521b24c6722f21a4bf8ce72bc971058af7
Harun Tamokur <h_tamokur@hotmail.com>
2025-02-06 11:10:12 +0300

    6. ders dahil edildi. Diğer eğitime geçiliyor. İlerleyen zamanlarda buraya tekrar geçilerek devam edilecektir.

---
+++ _06_Arrays/Program.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Temel Dizi Örnekleri
+            //2.4.6.8
+            //sarı,kırmızı,mavi,turuncu,beyaz
+            //adana,ankara,istanbul,bursa
+            //Değişken Türü[] DiziAdi=new DeğişkenTürü[ElemanSayısı]
+            //string[] colors = new string[4];
+            //colors[0] = "Kırmızı";
+            //colors[1] = "Sarı";
+            //colors[2] = "Beyaz";
+            //colors[3] = "Mavi";
+
+            //Console.WriteLine(colors[2]);
+
+            //string[] cities = new string[5];
+
+            //cities[0] = "Milano";
+            //cities[1] = "Budapeşte";
+            //cities[2] = "Lyon";
+            //cities[3] = "Kahire";
+            //cities[4] = "Üsküp";
+
+            //Console.WriteLine(cities[4]);
+
+            //int[] numbers = new int[10];
+
+            //numbers[0] = 50;
+            //numbers[1] = 48;
+            //numbers[2] = 698;
+            //numbers[3] = 24;
+            //numbers[4] = 748;
+            ////NOT!! bir index tam sayı formatında int iken yani değer ataması 
+            ////yapılmazsa o index değeri 0 olur. 
+            //Console.WriteLine(numbers[5]);
+            //string[] cities = { "Prag", "Roma", "Ankara", "Bursa" };
+
+            //Console.WriteLine(cities[2]);
+
+            #endregion
+
+            #region Dizideki Tüm Elemanları Listeleme
+            //string[] colors = { "Sarı", "Kırmızı", "Beyaz", "Mavi", "Yeşil", "Turuncu", "Pembe" };
+            //for (int i = 0; i < colors.Length; i++)
+            //{
+            //    Console.WriteLine(colors[i]);
+            //}
+
+            //Dizi içindeki 3 e tam bölünen sayılar
+
+            //int[] numbers = { 4, 85, 96, 75, 125, 635, 488, 522, 7456, 2365, 1120 };
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //        if (numbers[i]%3==0) 
+            //        { 
+            //            Console.WriteLine(numbers[i]);
+            //        }
+            //}
+
+            //char[] symbols = { 'a', 'b', 'c', '*', '/', '-' };
+            //for (int i = 0; i < symbols.Length; i++)
+            //{
+            //    Console.WriteLine(symbols[i]);
+            //}
+
+            //int[] myArray = {47,85,95,41,25,635,789,86,100 };
+
+            //int maxNumber = myArray[0];
+
+            //for (int i = 0; i < myArray.Length; i++)
+            //{
+            //    if (myArray[i]>maxNumber) 
+            //    {
+            //        maxNumber = myArray[i];
+            //    }
+            //}
+            //Console.WriteLine(maxNumber);
+
+            //string[] persons = { "ali", "ahmet", "ayşe", "buse", "cem", "deniz" };
+
+            //Console.WriteLine(persons.Length);
+
+            //int[] numbers = { 45, 85, 52, 41, 86, 10, 22, 35 };
+            //Array.Sort(numbers);
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+            //Reverse indisi tersten sıralıyor
+            //mesela A[0]A[1]A[2]...A[10] şeklinde sırlanan diziyi
+            //reverse kullandığımız A[10]A[9]...A[2]A[1]A[0] şeklinde sıralıyoruz
+
+            //int[] numbers = { 45, 85, 52, 41, 86, 10, 22, 35 };
+            //Array.Reverse(numbers);
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+
+
+            #endregion
+
+            #region Dizi metotlar
+
+            //string[] customers = { "ali", "buse", "ayşegül", "merve", "çınar", "kaya" };
+
+            ////merve elemanının kaçıncı indiste olduğunu aşağıdaki gibi alabiliriz. 
+            //int index = Array.IndexOf(customers, "merve");
+            //Console.WriteLine(index);
+
+            //int[] numbers = { 45, 85, 96, 63, 74, 10, 25, 22, 36 };
+
+            //Console.WriteLine("Dizinin en büyük elemanı: "+numbers.Max()+
+            //    " Dizinin En küçük elemanı: "+numbers.Min());
+
+
+
+            #endregion
+
+
+            #region Kullanıcı Değer Alma
+
+            //string[] cities = new string[5];
+
+            //for (int i = 0; i < cities.Length; i++)
+            //{
+            //    Console.WriteLine($"Lütfen {i+1}. Şehri Giriniz: ");
+            //    cities[i] = Console.ReadLine();
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("------------------------------");
+
+            //for (int i = 0; i < cities.Length; i++)
+            //{
+            //    Console.WriteLine(cities[i]);
+            //}
+
+            //int[] numbers = { 10, 20, 30, 40, 50 };
+            //int sum = 0;
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    sum += numbers[i];
+
+            //}
+            //Console.WriteLine(sum);
+
+            int[] numbers = { 21, 42, 33, 54, 55, 66, 897, 748, 39, 220 };
+
+            Console.WriteLine("Çift sayılar");
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i]%2==0) 
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Tek sayılar");
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 1)
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+
+
+            #endregion
+
+            Console.Read();
+
+        }
+    }
+}
